11e9eb50b90cb49629874a5c4a030fc0379841a3
yuqiyang <yuqiyang@ebay.com>
2019-08-26 16:09:37 +0800

    init project

---
+++ src/components/DescriptionGroup.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-row class="description-group">
+    <slot style="width: 100%"></slot>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'DescriptionGroup',
+};
+</script>
+
+<style scoped>
+.description-group {
+  width: 98%;
+  margin: 1%;
+  border: #e2e2e2 1px solid;
+}
+</style>
---
+++ src/components/DescriptionItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <b-col :cols="span">
+      <b-row class="item">
+          <b-col class="text label-background" :cols="24/span">
+            <strong>{{upperCaseLabel}}</strong>
+          </b-col>
+          <b-col class="text">
+            <slot></slot>
+          </b-col>
+      </b-row>
+  </b-col>
+</template>
+
+<script>
+export default {
+  name: 'DescriptionItem',
+  mounted() {
+  },
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    span: {
+      type: Number,
+      default: 4,
+    },
+  },
+  computed: {
+    upperCaseLabel() {
+      return this.label.split('_').map(l => l.charAt(0).toUpperCase() + l.slice(1)).join(' ');
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .item {
+    font-size: 14px;
+    height: 3.5rem;
+    border-bottom: #e2e2e2 1px solid;
+  }
+  .label-background {
+    background-color: #e9ecef;
+  }
+  .text {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
